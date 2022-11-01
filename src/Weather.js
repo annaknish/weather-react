2a415552d627293caf7aeb646f83cca9d244bfdb
annaknish <108184361+annaknish@users.noreply.github.com>
2022-11-01 16:56:46 +0200

    Integrated bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,64 @@
-import React from "react";
-import axios from "axios";
-import { Audio } from "react-loader-spinner";
+import "./App.css";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
-  }
-  let apiKey = `cb94f4da5c575e994b3b723a98782b7b`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    temperature: 19,
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
+
   return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="clearfix weather-temperature">
+            <img src={weatherData.imgUrl} alt="Sunny" className="float-left" />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°С</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
